ba53857013e5ff3ad9bb2ed43e06123a442b8ee0
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-07-12 19:48:53 +0300

    Cleanup repository and setup basic/blank application

---
+++ src/components/App/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return <div>Clean Oil Uganda Ltd.</div>;
+  }
+}
+ 
+export default App;
