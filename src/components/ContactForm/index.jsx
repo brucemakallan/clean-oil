4378027c3c5a20e4a920448de21a06ffb52a02ba
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-07-26 16:22:04 +0300

    Connect node mailer to the contact form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './contactForm.scss';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import sendEmail from '../../redux/actions/emailActions';
+import { endpoints } from '../../common';
 
 const formFields = [
   {
@@ -19,28 +23,80 @@ const formFields = [
   },
 ];
 
-const ContactForm = () => (
-  <div className="contact-form">
-    <form>
-      {formFields.map(field => (
-        <div key={field.id} className="form-group">
-          <input
-            type={field.type}
-            name={field.id}
-            id={field.id}
-            placeholder={field.placeholder}
-            className="form-control"
-          />
-        </div>
-      ))}
-      <div className="form-group">
-        <textarea className="form-control" rows={5} placeholder="Message" />
-      </div>
-      <button type="submit" className="btn btn-primary">
-        SEND EMAIL
-      </button>
-    </form>
-  </div>
-);
+class ContactForm extends Component {
+  state = {
+    fullName: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
 
-export default ContactForm;
+  onChange = (e) => {
+    e.preventDefault();
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const {
+      fullName, email, subject, message,
+    } = this.state;
+    const { sendEmailDispatch } = this.props;
+    const body = {
+      name: fullName,
+      email,
+      subject,
+      message,
+    };
+    sendEmailDispatch(endpoints().sendEmail, body);
+  }
+
+  render() {
+    return (
+      <div className="contact-form">
+        <form onSubmit={this.onSubmit}>
+          {formFields.map(field => (
+            <div key={field.id} className="form-group">
+              <input
+                type={field.type}
+                name={field.id}
+                id={field.id}
+                placeholder={field.placeholder}
+                onChange={this.onChange}
+                required
+                className="form-control"
+              />
+            </div>
+          ))}
+          <div className="form-group">
+            <textarea
+              name="message"
+              id="message"
+              className="form-control"
+              rows={5}
+              placeholder="Message"
+              required
+              onChange={this.onChange}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            SEND EMAIL
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  sendEmailDispatch: PropTypes.func.isRequired,
+};
+const mapStateToProps = state => (state);
+const mapDispatchToProps = {
+  sendEmailDispatch: sendEmail,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactForm);
